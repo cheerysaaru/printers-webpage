438153548ae0c0892842aea166c8c472279e2e94
cheerysaaru <cheerysaark@gmail.com>
2026-08-27 14:38:17 +0800

    Deploy frontend to GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
